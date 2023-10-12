a2519cdb0de8a719bd3c2a77a1180cbf954ef8c6
alnsousa1 <alnprivado@gmail.com>
2023-10-11 21:16:28 -0300

    criando o consumer queue e gerando o arquivo de logs de emails

--- consumer_queue.php
+++ consumer_queue.php
@@ -12,5 +12,17 @@ $emailQueue = "email_queue";
 while(true){
     if($redisQueue->getQueueLength($emailQueue) > 0){
         $orderData = $redisQueue->dequeue($emailQueue);
+
+        //o parametro TRUE formata o conteudo do arquivo emails.log
+        $order = json_decode($orderData, true);
+        $email = $order["email"];
+
+        //FILE_APPEND insere um arquivo, porem, sempre na proxima linha, ou seja, nao substitui
+        file_put_contents('emails.log', print_r($order, true), FILE_APPEND);
+
+        echo "Email enviado: $email <br>";
+    }else{
+        echo "a fila de emails está vazia. Aguardando novos pedidos... <br>";
+        sleep(5);
     }
 }
