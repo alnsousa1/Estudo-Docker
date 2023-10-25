1c86c64bab4d990ebe208696b3065d30c6942489
alnsousa1 <alnprivado@gmail.com>
2023-10-25 17:14:01 -0300

    adicionando comentários explicando o que cada linha está fazendo

--- src/OrderProcessor.php
+++ src/OrderProcessor.php
@@ -1,8 +1,10 @@
 <?php
-
+//declarando um namespace Unialfa, usado para organizar as classes em grupos lógicos. As classes que forem definidas dentro desse namespace devem ser acessadas utilizando Unialfa\
 namespace Unialfa;
 
+//Criando a classe OrderProcessor, responsável pelo processamento de pedidos
 class OrderProcessor {
+    //criando uma propriedade privada que será usada para armazenar uma instância da classe RedisQueue.
     private $queue;
 
     public function __construct(RedisQueue $queue)
